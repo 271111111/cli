604c7452dabd08de6193d881c7a922d2b9928cf1
Reggi <reggi@github.com>
2025-06-24 10:42:13 -0400

    fix: oidc idp token as header, exchange endpoint change (#8370)

--- mock-registry/lib/index.js
+++ mock-registry/lib/index.js
@@ -632,10 +632,10 @@ class MockRegistry {
   }
 
   mockOidcTokenExchange ({ packageName, idToken, token, statusCode = 200 } = {}) {
-    this.nock.post(this.fullPath('/-/npm/v1/oidc/token/exchange'), body => {
-      return body.package_name === packageName && body.id_token === idToken
-    }).reply(statusCode, statusCode !== 500 ? { token: token } : { message: 'Internal Server Error' })
-    return { token }
+    const encodedPackageName = npa(packageName).escapedName
+    this.nock.post(this.fullPath(`/-/npm/v1/oidc/token/exchange/package/${encodedPackageName}`))
+      .matchHeader('authorization', `Bearer ${idToken}`)
+      .reply(statusCode, statusCode !== 500 ? { token } : { message: 'Internal Server Error' })
   }
 }
 
